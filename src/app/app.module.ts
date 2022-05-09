fdb19a53e80f9d0caaaaf0892789dee9fdee32ff
Seunghyun-Kang <kiseyno@gmail.com>
2022-05-10 00:02:33 +0900

    Add app.module / app.compoennt.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HomeComponent } from './components/home/home.component';
+import { FirstsurveyComponent } from './components/firstsurvey/firstsurvey.component';
+import { SecondsurveyComponent } from './components/secondsurvey/secondsurvey.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HomeComponent,
+    FirstsurveyComponent,
+    SecondsurveyComponent,
   ],
   imports: [
     BrowserModule,
