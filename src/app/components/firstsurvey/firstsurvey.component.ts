80c6d8e5a378670a9dc3c7b53013f3273c9b4982
Seunghyun-Kang <kiseyno@gmail.com>
2022-05-10 21:56:06 +0900

    Add request service in component

--- src/app/components/firstsurvey/firstsurvey.component.ts
+++ src/app/components/firstsurvey/firstsurvey.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from "@angular/core";
 import {ErrorStateMatcher} from '@angular/material/core';
 import { FormBuilder, FormGroup, Validators,FormControl, FormGroupDirective, NgForm, } from '@angular/forms';
+import { RequestService } from 'src/app/services/request.service';
 export class MyErrorStateMatcher implements ErrorStateMatcher {
   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
     const isSubmitted = form && form.submitted;
@@ -23,7 +24,9 @@ export class FirstsurveyComponent implements OnInit {
   public emailFormControl = new FormControl('', [Validators.required, Validators.email]);
   public NumberFormControl = new FormControl('', [Validators.required, Validators.min(1)]);
   public IS_MOBILE = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-  constructor(private _formBuilder: FormBuilder) {
+  
+  constructor(private _formBuilder: FormBuilder,
+    private requestService: RequestService,) {
   }
 
   ngOnInit() {
@@ -65,28 +68,68 @@ export class FirstsurveyComponent implements OnInit {
   }
 
   onClickComplete() {
-    console.log("name: " + this.userFormGroup.controls['nameCtrl'].value)
-    console.log("birth: " + this.userFormGroup.controls['birthCtrl'].value)
-    console.log("email: " + this.userFormGroup.controls['emailCtrl'].value)
+    // console.log("name: " + this.userFormGroup.controls['nameCtrl'].value)
+    // console.log("birth: " + this.userFormGroup.controls['birthCtrl'].value)
+    // console.log("email: " + this.userFormGroup.controls['emailCtrl'].value)
 
-    console.log("samsung_card: " + this.payFormGroup.controls['samsung_card'].value)
-    console.log("shinhan_card: " + this.payFormGroup.controls['shinhan_card'].value)
-    console.log("hyundai_card: " + this.payFormGroup.controls['hyundai_card'].value)
+    // console.log("samsung_card: " + this.payFormGroup.controls['samsung_card'].value)
+    // console.log("shinhan_card: " + this.payFormGroup.controls['shinhan_card'].value)
+    // console.log("hyundai_card: " + this.payFormGroup.controls['hyundai_card'].value)
 
-    console.log("skt_mc: " + this.payFormGroup.controls['skt_mc'].value)
-    console.log("kt_mc: " + this.payFormGroup.controls['kt_mc'].value)
-    console.log("lg_mc: " + this.payFormGroup.controls['lg_mc'].value)
-    console.log("extra_mc: " + this.payFormGroup.controls['extra_mc'].value)
-    console.log("vip_mc: " + this.payFormGroup.controls['vip_mc'].value)
+    // console.log("skt_mc: " + this.payFormGroup.controls['skt_mc'].value)
+    // console.log("kt_mc: " + this.payFormGroup.controls['kt_mc'].value)
+    // console.log("lg_mc: " + this.payFormGroup.controls['lg_mc'].value)
+    // console.log("extra_mc: " + this.payFormGroup.controls['extra_mc'].value)
+    // console.log("vip_mc: " + this.payFormGroup.controls['vip_mc'].value)
     
-    console.log("kakao_pay: " + this.payFormGroup.controls['kakao_pay'].value)
-    console.log("payco_pay: " + this.payFormGroup.controls['payco_pay'].value)
-    console.log("smile_pay: " + this.payFormGroup.controls['smile_pay'].value)
-    console.log("naver_pay: " + this.payFormGroup.controls['naver_pay'].value)
+    // console.log("kakao_pay: " + this.payFormGroup.controls['kakao_pay'].value)
+    // console.log("payco_pay: " + this.payFormGroup.controls['payco_pay'].value)
+    // console.log("smile_pay: " + this.payFormGroup.controls['smile_pay'].value)
+    // console.log("naver_pay: " + this.payFormGroup.controls['naver_pay'].value)
     
-    console.log("passenger num: " + this.flightFormGroup.controls['passengerCtrl'].value)
-    console.log("travel start date: " + this.flightFormGroup.controls['travelstartdateCtrl'].value)
-    console.log("travel end date: " + this.flightFormGroup.controls['travelenddateCtrl'].value)
+    // console.log("passenger num: " + this.flightFormGroup.controls['passengerCtrl'].value)
+    // console.log("travel start date: " + this.flightFormGroup.controls['travelstartdateCtrl'].value)
+    // console.log("travel end date: " + this.flightFormGroup.controls['travelenddateCtrl'].value)
 
+    if(this.flightFormGroup.controls['passengerCtrl'].value !== undefined &&
+    this.flightFormGroup.controls['travelstartdateCtrl'].value !== undefined &&
+    this.flightFormGroup.controls['travelenddateCtrl'].value !== undefined) {
+      console.log("REQUEST SEND")
+      this.requestService.getEvent({
+        "user_info": {
+          "name": this.userFormGroup.controls['nameCtrl'].value,
+          "birthday": this.userFormGroup.controls['birthCtrl'].value,
+          "email": this.userFormGroup.controls['emailCtrl'].value
+        },
+        "pay_info": {
+          "samsung_card": this.payFormGroup.controls['samsung_card'].value,
+          "hyundai_card": this.payFormGroup.controls['hyundai_card'].value,
+          "shinhan_card": this.payFormGroup.controls['shinhan_card'].value,
+
+          "skt_mc": this.payFormGroup.controls['skt_mc'].value,
+          "kt_mc": this.payFormGroup.controls['kt_mc'].value,
+          "lg_mc": this.payFormGroup.controls['lg_mc'].value,
+          "extra_mc": this.payFormGroup.controls['extra_mc'].value,
+          "vip_mc": this.payFormGroup.controls['vip_mc'].value,
+
+          "kakao_pay": this.payFormGroup.controls['kakao_pay'].value,
+          "payco_pay": this.payFormGroup.controls['payco_pay'].value,
+          "smile_pay": this.payFormGroup.controls['smile_pay'].value,
+          "naver_pay": this.payFormGroup.controls['naver_pay'].value
+
+        },
+        "flight_info": {
+          "passenger_num": this.flightFormGroup.controls['passengerCtrl'].value,
+          "start_date": this.flightFormGroup.controls['travelstartdateCtrl'].value,
+          "end_date": this.flightFormGroup.controls['travelenddateCtrl'].value,
+        }
+      }, "airline").subscribe({
+        next: (v: any) => {
+          console.log("RESPONSE CAME")
+          console.log(Object(v.body))
+        },
+        error: (e: any) => console.log("ERROR OCCURED :: " + JSON.stringify(e))
+      });
+    }
   }
 }
