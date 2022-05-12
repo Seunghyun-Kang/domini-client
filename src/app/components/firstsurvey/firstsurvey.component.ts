fb982e106f5c9c090546dd16375138c75ec789b2
Seunghyun-Kang <kiseyno@gmail.com>
2022-05-12 18:29:45 +0900

    Add event showing logic

--- src/app/components/firstsurvey/firstsurvey.component.ts
+++ src/app/components/firstsurvey/firstsurvey.component.ts
@@ -2,6 +2,17 @@ import { Component, Input, OnInit } from "@angular/core";
 import {ErrorStateMatcher} from '@angular/material/core';
 import { FormBuilder, FormGroup, Validators,FormControl, FormGroupDirective, NgForm, } from '@angular/forms';
 import { RequestService } from 'src/app/services/request.service';
+
+export interface IEvent {
+    airline: string,
+    discount: number,
+    credit_card: string,
+    pay_application: string,
+    min_cost: number,
+    start_date: string,
+    end_date: string
+}
+
 export class MyErrorStateMatcher implements ErrorStateMatcher {
   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
     const isSubmitted = form && form.submitted;
@@ -21,6 +32,9 @@ export class FirstsurveyComponent implements OnInit {
   payFormGroup: FormGroup;
   flightFormGroup: FormGroup;
   matcher = new MyErrorStateMatcher();
+  public getData = false
+  public matchEventList: Array<IEvent> = []
+  public oneStepEventList: Array<IEvent> = []
   public emailFormControl = new FormControl('', [Validators.required, Validators.email]);
   public NumberFormControl = new FormControl('', [Validators.required, Validators.min(1)]);
   public IS_MOBILE = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
@@ -68,29 +82,6 @@ export class FirstsurveyComponent implements OnInit {
   }
 
   onClickComplete() {
-    // console.log("name: " + this.userFormGroup.controls['nameCtrl'].value)
-    // console.log("birth: " + this.userFormGroup.controls['birthCtrl'].value)
-    // console.log("email: " + this.userFormGroup.controls['emailCtrl'].value)
-
-    // console.log("samsung_card: " + this.payFormGroup.controls['samsung_card'].value)
-    // console.log("shinhan_card: " + this.payFormGroup.controls['shinhan_card'].value)
-    // console.log("hyundai_card: " + this.payFormGroup.controls['hyundai_card'].value)
-
-    // console.log("skt_mc: " + this.payFormGroup.controls['skt_mc'].value)
-    // console.log("kt_mc: " + this.payFormGroup.controls['kt_mc'].value)
-    // console.log("lg_mc: " + this.payFormGroup.controls['lg_mc'].value)
-    // console.log("extra_mc: " + this.payFormGroup.controls['extra_mc'].value)
-    // console.log("vip_mc: " + this.payFormGroup.controls['vip_mc'].value)
-    
-    // console.log("kakao_pay: " + this.payFormGroup.controls['kakao_pay'].value)
-    // console.log("payco_pay: " + this.payFormGroup.controls['payco_pay'].value)
-    // console.log("smile_pay: " + this.payFormGroup.controls['smile_pay'].value)
-    // console.log("naver_pay: " + this.payFormGroup.controls['naver_pay'].value)
-    
-    // console.log("passenger num: " + this.flightFormGroup.controls['passengerCtrl'].value)
-    // console.log("travel start date: " + this.flightFormGroup.controls['travelstartdateCtrl'].value)
-    // console.log("travel end date: " + this.flightFormGroup.controls['travelenddateCtrl'].value)
-
     if(this.flightFormGroup.controls['passengerCtrl'].value !== undefined &&
     this.flightFormGroup.controls['travelstartdateCtrl'].value !== undefined &&
     this.flightFormGroup.controls['travelenddateCtrl'].value !== undefined) {
@@ -125,8 +116,11 @@ export class FirstsurveyComponent implements OnInit {
         }
       }, "airline").subscribe({
         next: (v: any) => {
+          this.getData = true
           console.log("RESPONSE CAME")
           console.log(Object(v.body))
+          this.matchEventList = Object(v.body)['match']
+          this.oneStepEventList = Object(v.body)['onestep']
         },
         error: (e: any) => console.log("ERROR OCCURED :: " + JSON.stringify(e))
       });
